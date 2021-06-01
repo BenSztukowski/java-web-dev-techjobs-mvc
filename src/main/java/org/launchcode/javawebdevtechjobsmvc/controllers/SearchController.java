0383e97557e60659ca1fe6417fab7cdae2748e19
Ben <bensztukowski@gmail.com>
2021-06-01 18:57:59 -0500

    Finished assignment; no bonuses

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
@@ -23,6 +24,20 @@ public class SearchController {
         return "search";
     }
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs = new ArrayList<>();
+        if (searchType.toLowerCase().equals("all")){
+            model.addAttribute("title", "All Jobs containing: " + searchTerm);
+        } else {
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
